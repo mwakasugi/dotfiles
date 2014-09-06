7063431ba85cf335250885205f15fa43bde03d97
WAKASUGI Mubae <baenej@gmail.com>
2014-09-06 23:07:17 +0900

    Updated

--- .emacs.d/config/addons.el
+++ .emacs.d/config/addons.el
@@ -184,6 +184,11 @@
 ;;
 ;; yaml-mode
 
+;;
+;; dash-at-point
+(global-set-key "\C-cd" 'dash-at-point)
+(global-set-key "\C-ce" 'dash-at-point-with-docset)
+
 ;; =====================================================================
 ;; Addon key bindings
 ;; =====================================================================
