940645830efa3b9ae95fe4e559ed9db6cf21b9f4
WAKASUGI Mubae <baenej@gmail.com>
2014-09-14 15:37:37 +0900

    Updated.

--- .emacs.d/config/addons.el
+++ .emacs.d/config/addons.el
@@ -208,6 +208,20 @@
 (global-set-key "\C-cd" 'dash-at-point)
 (global-set-key "\C-ce" 'dash-at-point-with-docset)
 
+;; scss-mode
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(defun scss-custom ()
+  "scss-mode-hook"
+  (and
+   (set (make-local-variable 'css-indent-offset) 2)
+   (set (make-local-variable 'scss-compile-at-save) nil)
+   )
+  )
+(add-hook 'scss-mode-hook
+  '(lambda() (scss-custom)))
+
+;; sass-mode
+
 ;; =====================================================================
 ;; Addon key bindings
 ;; =====================================================================
