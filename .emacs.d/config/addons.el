1758eb3950447fef6d5fc4e8f9078075f6f3cbf8
WAKASUGI Mubae <baenej@gmail.com>
2014-09-18 19:21:29 +0900

    Updated.

--- .emacs.d/config/addons.el
+++ .emacs.d/config/addons.el
@@ -155,6 +155,20 @@
 ;; helm-git-grep
 
 ;;
+;; helm-gtags
+(require 'helm-gtags)
+(add-hook 'python-mode-hook (lambda () (helm-gtags-mode)))  
+(add-hook 'ruby-mode-hook (lambda () (helm-gtags-mode)))                         
+(setq helm-gtags-path-style 'root)                       
+(setq helm-gtags-auto-update t)
+(add-hook 'helm-gtags-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-g") 'helm-gtags-dwim)
+             (local-set-key (kbd "M-s") 'helm-gtags-show-stack)
+             (local-set-key (kbd "M-p") 'helm-gtags-previous-history)
+             (local-set-key (kbd "M-n") 'helm-gtags-next-history)))    
+
+;;
 ;; enhanced-ruby-mode
 ;;(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 ;;(add-to-list 'auto-mode-alist '("Capfile$" . enh-ruby-mode))
