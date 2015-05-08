51e9151a22ada571645ba7c70a5a23d7f06926ef
Hirofumi Wakasugi <baenej@gmail.com>
2015-05-08 11:00:38 +0900

    update

--- .emacs.d/config/addons.el
+++ .emacs.d/config/addons.el
@@ -480,6 +480,14 @@
 ;; =====================================================================
 ;; php-mode
 ;; =====================================================================
+(add-hook 'php-mode-hook
+  '(lambda()
+     (c-set-offset 'arglist-intro '+)
+     (c-set-offset 'arglist-close 0)
+     (setq tab-width 4)
+     (setq indent-tabs-mode t)
+     (setq c-basic-offset 4)
+   ))
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
 ;; =====================================================================
