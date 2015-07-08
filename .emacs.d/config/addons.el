0e8b785752ee3c3c27e836b79b0ad561490be767
Hirofumi Wakasugi <baenej@gmail.com>
2015-07-08 15:55:25 +0900

    update

--- .emacs.d/config/addons.el
+++ .emacs.d/config/addons.el
@@ -496,6 +496,18 @@
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
 ;; =====================================================================
+;; elixir-mode
+;; =====================================================================
+(add-hook 'elixir-mode-hook '(lambda ()
+			       (local-set-key "\\t" 'elixir-mode-indent-line)
+			       (make-local-variable 'default-tab-width)
+			       (setq default-tab-width 2)
+			       (setq indent-tabs-mode nil)
+			       (local-set-key "\C-c\C-l" 'elixir-mode-iex)))
+(add-to-list 'auto-mode-alist '("\\.ex$" . elixir-mode))
+(add-to-list 'auto-mode-alist '("\\.exs$" . elixir-mode))
+
+;; =====================================================================
 ;; web-mode
 ;; =====================================================================
 (add-to-list 'auto-mode-alist '("\\.ctp$" . web-mode))
