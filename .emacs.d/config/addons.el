5a55b685844ffbc57b157945514c573903b1a1ef
Hirofumi Wakasugi <baenej@gmail.com>
2015-08-19 10:44:41 +0900

    Update

--- .emacs.d/config/addons.el
+++ .emacs.d/config/addons.el
@@ -526,6 +526,7 @@
 ;; =====================================================================
 ;; web-mode
 ;; =====================================================================
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ctp$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
@@ -534,6 +535,12 @@
       (let ((web-mode-enable-part-face nil))
         ad-do-it)
     ad-do-it))
+(add-hook 'web-mode-hook
+	  '(lambda ()
+	     (setq web-mode-markup-indent-offset 2)
+	     (setq web-mode-css-indent-offset 2)
+	     (setq web-mode-code-indent-offset 2)
+	     (setq indent-tabs-mode nil)))
 
 ;; =====================================================================
 ;; js2-mode
