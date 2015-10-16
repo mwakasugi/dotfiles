786403d22134ea35b8054196fa53bda360bb8751
Hirofumi Wakasugi <baenej@gmail.com>
2015-10-16 20:45:46 +0900

    Update

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $HOME
+ln -s Laboratory/dotfiles/.zshrc
+ln -s Laboratory/dotfiles/.zshlib
+ln -s Laboratory/dotfiles/.zshenv
+ln -s Laboratory/dotfiles/.vimrc
+ln -s Laboratory/dotfiles/.gvimrc
+ln -s Laboratory/dotfiles/.vim
+ln -s Laboratory/dotfiles/.emacs.d
+ln -s Laboratory/dotfiles/.gemrc
+ln -s Laboratory/dotfiles/.gitignore_global
+ln -s Laboratory/dotfiles/.pryrc
+ln -s Laboratory/dotfiles/.rubocop.yml
+ln -s Laboratory/dotfiles/.tmux.conf
