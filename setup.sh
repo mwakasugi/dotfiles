256ecb95a6d3041cf50228b678274967aebb1901
Hirofumi Wakasugi <baenej@gmail.com>
2015-11-04 02:51:13 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -11,5 +11,6 @@ ln -s Laboratory/dotfiles/.emacs.d
 ln -s Laboratory/dotfiles/.gemrc
 ln -s Laboratory/dotfiles/.gitignore_global
 ln -s Laboratory/dotfiles/.pryrc
+ln -s Laboratory/dotfiles/.tigrc
 ln -s Laboratory/dotfiles/.rubocop.yml
 ln -s Laboratory/dotfiles/.tmux.conf
