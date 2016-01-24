79a0baee2f0cf8a5cafa2ac9b4e1f2d40e74d295
Hirofumi Wakasugi <baenej@gmail.com>
2016-01-24 20:13:37 +0900

    Update

--- .emacs.d/inits/30_visual-regexp-steroids.el
+++ .emacs.d/inits/30_visual-regexp-steroids.el
@@ -3,8 +3,8 @@
 
 (require 'visual-regexp-steroids)
 
-(setq vr/engine 'python)
-;; (setq vr/engine 'pcre2el)
+;; (setq vr/engine 'python)
+(setq vr/engine 'pcre2el)
 (global-set-key (kbd "M-%") 'vr/query-replace)
 ;; When using multiple-cursors
 ;; (global-set-key (kbd "C-c m") 'vr/mc-mark)
