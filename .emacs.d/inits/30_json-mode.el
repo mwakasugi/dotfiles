44d6784b0cceb83338140132fd0eb5fb78ef0178
Hirofumi Wakasugi <baenej@gmail.com>
2016-02-23 12:57:41 +0900

    update

--- .emacs.d/inits/30_json-mode.el
+++ .emacs.d/inits/30_json-mode.el
@@ -2,9 +2,9 @@
 
 ;; Overwrite json-mode-beautify to preserve the original unicode content with Python.
 (defconst json-mode-beautify-command-python2
-  "python2 -c \"import sys,json,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print json.dumps(data,sort_keys=%s,indent=4,separators=(',',': ')).decode('unicode_escape').encode('utf8','replace')\"")
+  "python2 -c \"import sys,json,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print json.dumps(data,sort_keys=%s,indent=2,separators=(',',': ')).decode('unicode_escape').encode('utf8','replace')\"")
 (defconst json-mode-beautify-command-python3
-  "python3 -c \"import sys,json,codecs,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print((codecs.getdecoder('unicode_escape')(json.dumps(data,sort_keys=%s,indent=4,separators=(',',': '))))[0])\"")
+  "python3 -c \"import sys,json,codecs,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print((codecs.getdecoder('unicode_escape')(json.dumps(data,sort_keys=%s,indent=2,separators=(',',': '))))[0])\"")
 
 (eval-after-load "json-mode"
   '(defun json-mode-beautify (&optional preserve-key-order)
