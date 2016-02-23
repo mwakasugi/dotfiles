ae24089a652929501c6c4f2e890e66843a3faf81
Hirofumi Wakasugi <baenej@gmail.com>
2016-02-23 13:23:16 +0900

    update

--- .emacs.d/inits/30_json-mode.el
+++ .emacs.d/inits/30_json-mode.el
@@ -3,8 +3,14 @@
 ;; Overwrite json-mode-beautify to preserve the original unicode content with Python.
 (defconst json-mode-beautify-command-python2
   "python2 -c \"import sys,json,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print json.dumps(data,sort_keys=%s,indent=2,separators=(',',': ')).decode('unicode_escape').encode('utf8','replace')\"")
+(defconst json-mode-beautify-command-python2-with-unicode-escape
+  "python2 -c \"import sys,json,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print json.dumps(data,sort_keys=%s,indent=2,separators=(',',': '))\"")
+; The following Python 3 one-liner seems to contain bugs... Use Python2.
 (defconst json-mode-beautify-command-python3
   "python3 -c \"import sys,json,codecs,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print((codecs.getdecoder('unicode_escape')(json.dumps(data,sort_keys=%s,indent=2,separators=(',',': '))))[0])\"")
+(defconst json-mode-beautify-command-python3-with-unicode-escape
+  "python3 -c \"import sys,json,codecs,collections; data=json.loads(sys.stdin.read(),object_pairs_hook=collections.OrderedDict); print((json.dumps(data,sort_keys=%s,indent=2,separators=(',',': ')))[0])\"")
+
 
 (eval-after-load "json-mode"
   '(defun json-mode-beautify (&optional preserve-key-order)
@@ -16,6 +22,18 @@
                                       (format (if (executable-find "python2")
                                                   json-mode-beautify-command-python2
                                                 json-mode-beautify-command-python3)
+                                              (if preserve-key-order "False" "True")))
+                              (current-buffer) t)))
+(eval-after-load "json-mode"
+  '(defun json-mode-beautify-with-unicode-escape (&optional preserve-key-order)
+     "Beautify / pretty-print from BEG to END, and optionally PRESERVE-KEY-ORDER."
+     (interactive "P")
+     (shell-command-on-region (if (use-region-p) (region-beginning) (point-min))
+                              (if (use-region-p) (region-end) (point-max))
+                              (concat (if (executable-find "env") "env " "")
+                                      (format (if (executable-find "python2")
+                                                  json-mode-beautify-command-python2-with-unicode-escape
+                                                json-mode-beautify-command-python3-with-unicode-escape)
                                               (if preserve-key-order "False" "True")))
                               (current-buffer) t)))
 
