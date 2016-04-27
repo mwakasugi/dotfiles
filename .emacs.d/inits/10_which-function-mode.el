2778a2dece6fe5f843577e01b1d64eaf5334473a
Hirofumi Wakasugi <baenej@gmail.com>
2016-04-27 14:59:55 +0900

    Update

--- .emacs.d/inits/10_which-function-mode.el
+++ .emacs.d/inits/10_which-function-mode.el
@@ -1,16 +1,19 @@
-(setq which-func-unknown "n/a")
-
-;; Show the current function name in the header line
 (which-function-mode)
-(setq-default header-line-format
-              '((which-func-mode ("" which-func-format " "))))
-(setq mode-line-misc-info
-            ;; We remove Which Function Mode from the mode line, because it's mostly
-            ;; invisible here anyway.
-            (assq-delete-all 'which-func-mode mode-line-misc-info))
+
+(setq which-func-unknown "N/A")
+
+(setq mode-line-misc-info (delete (assoc 'which-func-mode
+                                      mode-line-misc-info) mode-line-misc-info)
+      which-func-header-line-format '(which-func-mode ("" which-func-format)))
+
+(defadvice which-func-ff-hook (after header-line activate)
+  (when which-func-mode
+    (setq mode-line-misc-info (delete (assoc 'which-func-mode
+                                          mode-line-misc-info) mode-line-misc-info)
+          header-line-format which-func-header-line-format)))
 
 (custom-set-faces
- '(which-func ((t (:foreground "#d33682")))))
+ '(which-func ((t (:foreground "rosy brown")))))
 
 (add-to-list 'which-func-modes 'php-mode)
 (add-to-list 'which-func-modes 'python-mode)
