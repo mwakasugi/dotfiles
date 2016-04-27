824f8de46d1764d8fdde1d4338feea5881eec4be
Hirofumi Wakasugi <baenej@gmail.com>
2016-04-27 15:02:15 +0900

    update

--- .emacs.d/inits/10_which-function-mode.el
+++ .emacs.d/inits/10_which-function-mode.el
@@ -12,9 +12,6 @@
                                           mode-line-misc-info) mode-line-misc-info)
           header-line-format which-func-header-line-format)))
 
-(custom-set-faces
- '(which-func ((t (:foreground "rosy brown")))))
-
 (add-to-list 'which-func-modes 'php-mode)
 (add-to-list 'which-func-modes 'python-mode)
 (add-to-list 'which-func-modes 'ruby-mode)
