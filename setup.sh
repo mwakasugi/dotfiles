04401d2722c0ba8a077a496ea6c82096f84d14ff
Hirofumi Wakasugi <baenej@gmail.com>
2016-04-28 15:23:24 +0900

    update

--- setup.sh
+++ setup.sh
@@ -16,3 +16,8 @@ ln -s Laboratory/dotfiles/.rubocop.yml
 ln -s Laboratory/dotfiles/.tmux.conf
 ln -s Laboratory/dotfiles/.atom
 ln -s Laboratory/dotfiles/.spacemacs
+
+# neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
