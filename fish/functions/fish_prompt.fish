a21362bc1ab8263c8213eb6febd998b4e3bd7b18
Hirofumi Wakasugi <baenej@gmail.com>
2016-07-27 01:06:45 +0900

    Update

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,1 +1,70 @@
-/Users/hirofumiwakasugi/.local/share/omf/themes/default/fish_prompt.fish
+# You can override some default options with config.fish:
+#
+#  set -g theme_short_path yes
+
+function fish_prompt
+  set -l last_command_status $status
+  set -l cwd
+
+  if test "$theme_short_path" = 'yes'
+    set cwd (basename (prompt_pwd))
+  else
+    set cwd (prompt_pwd)
+  end
+
+  # set -l fish     "⋊>"
+  set -l ahead    "↑"
+  set -l behind   "↓"
+  set -l diverged "⥄ "
+  set -l dirty    "⨯"
+  set -l none     "◦"
+
+  set -l normal_color     (set_color normal)
+  set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
+  set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
+  set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
+  set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
+
+  set -l emojis
+  set -l emojis $emojis 🐠
+  set -l emojis $emojis 🐟
+  set -l emojis $emojis 🐟
+  set -l emojis $emojis 🐡
+  set -l emojis $emojis 🐬
+  set -l emojis $emojis 🐳
+  set -l emojis $emojis 🐋
+  set -l emojis $emojis 🦀
+
+  set -l index (math (math (random)%(count $emojis))+1)
+  set -l fish $emojis[$index]
+
+  if test $last_command_status -eq 0
+    echo -n -s $success_color $fish $normal_color " "
+  else
+    echo -n -s $error_color 😇 $normal_color " "
+  end
+
+  if git_is_repo
+    if test "$theme_short_path" = 'yes'
+      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
+      set parent_root_folder (dirname $root_folder)
+      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+
+      echo -n -s " " $directory_color $cwd $normal_color
+    else
+      echo -n -s " " $directory_color $cwd $normal_color
+    end
+
+    echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
+
+    if git_is_touched
+      echo -n -s $dirty
+    else
+      echo -n -s (git_ahead $ahead $behind $diverged $none)
+    end
+  else
+    echo -n -s " " $directory_color $cwd $normal_color
+  end
+
+  echo -n -s " "
+end
