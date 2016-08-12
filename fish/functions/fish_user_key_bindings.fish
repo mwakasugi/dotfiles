f2e5f30a13cf69f63e56e991f5375ef16fe28bbe
Hirofumi Wakasugi <baenej@gmail.com>
2016-08-13 00:45:26 +0900

    Update

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,5 @@
 function fish_user_key_bindings
   bind \cr peco_select_history
   bind \cg peco_select_repository
+  bind \cb peco_git_checkout
 end
--- fish/functions/peco_git_checkout.fish
+++ fish/functions/peco_git_checkout.fish
@@ -3,10 +3,12 @@ function peco_git_checkout
 
   if [ $branch ]
     if echo $branch | grep -q -E '^.*remotes/.*$'
-    set -l b (echo $branch | awk -F'/' '{print $3}')
-    git checkout -b $b $branch
+      set -l b (echo $branch | awk -F'/' '{print $3}')
+      command git checkout -b $b $branch
+      commandline -f repaint
     else
       command git checkout $branch
+      commandline -f repaint
     end
   else
     commandline ''
