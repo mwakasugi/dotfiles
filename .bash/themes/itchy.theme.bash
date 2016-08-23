789c75249529bbe301db3eb37c013805596157c7
Hirofumi Wakasugi <baenej@gmail.com>
2016-08-23 13:02:27 +0900

    Update

--- .bash/themes/itchy.theme.bash
+++ .bash/themes/itchy.theme.bash
@@ -14,7 +14,7 @@ RBENV_THEME_PROMPT_PREFIX="("
 RBENV_THEME_PROMPT_SUFFIX=")"
 
 function prompt_command() {
-    PS1="\n${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${reset_color} \`
+    PS1="\n${yellow}$(ruby_version_prompt) ${cyan}\u${reset_color} @ ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${reset_color} \`
 if [ \$? = 0 ]; then
     echo 😉;
 else
