2ba2e3a360cd54c5bf79211d3360c81b2818c949
Hirofumi Wakasugi <baenej@gmail.com>
2016-11-28 12:56:59 +0900

    update

--- .zsh/key-bindings.zsh
+++ .zsh/key-bindings.zsh
@@ -1,5 +1,6 @@
 bindkey '^r' peco-select-history
 bindkey '^g' peco-select-repository
+bindkey '^[f' peco-find
 bindkey '^[k' peco-kill-process
 bindkey '^[o' peco-git-checkout
 bindkey '^[w' peco-select-git-worktree
