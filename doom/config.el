b527805b5b746e540b2fee17552d6d64d66872e5
Hirofumi Wakasugi <baenej@gmail.com>
2020-11-07 01:18:41 +0900

    Doom settings

--- doom/config.el
+++ doom/config.el
@@ -21,6 +21,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Fira Code" :size 12)
+      doom-big-font (font-spec :family "Fira Code" :size 24))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -52,3 +54,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(define-key key-translation-map [?\C-h] [?\C-?])
+(global-set-key (kbd "C-?") 'help-for-help)
+(setq-default line-spacing 0.3)
