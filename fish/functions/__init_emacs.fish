822f21eb95f2f01d2a546aeaf8ae108ce6837807
Hirofumi Wakasugi <baenej@gmail.com>
2020-11-13 21:45:11 +0900

    Update emacscient command to run multiple clients

--- fish/functions/__init_emacs.fish
+++ fish/functions/__init_emacs.fish
@@ -1,4 +1,4 @@
 function __init_emacs -d 'Get ready to use Emacs'
-    set -gx EDITOR "emacsclient --alternate-editor ''"
+    set -gx EDITOR "emacsclient --alternate-editor '' -nw"
     alias e $EDITOR
 end
